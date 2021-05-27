b4cc16f1e7e5baa418b21d5c55e3ef731fd6555f
Yohei Doi <63711662+DoiYohei@users.noreply.github.com>
2021-05-27 13:12:36 +0900

    Revert "Revert "Feature/#09 line up on rails""

--- app/models/band.rb
+++ app/models/band.rb
@@ -7,4 +7,5 @@ class Band < ActiveRecord::Base
   include DeviseTokenAuth::Concerns::User
 
   has_many :created_events, class_name: "Event", foreign_key: "owner_id", dependent: :nullify
+  has_many :performings, class_name: "Lineup", foreign_key: "performer_id", dependent: :nullfy
 end
