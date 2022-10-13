128853b1a19401c0b552484837d4288ccf1e624e
DoiYohei <doi-b.goode@docomo.ne.jp>
2022-10-13 13:31:46 +0900

    [fix] デプロイ時のエラー修正

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080' || 'http://stage-wing.com'
+    origins 'localhost:8080' || 'stage-wing.com'
 
     resource '*',
              headers: :any,
