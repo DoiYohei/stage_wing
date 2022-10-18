bdd8f4e906f0b80535cb00b4397ff2f774380101
DoiYohei <doi-b.goode@docomo.ne.jp>
2022-10-18 23:29:39 +0900

    [fix] 本番環境のチャット機能の設定を修正

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -112,6 +112,5 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   ActionCable.server.config.disable_request_forgery_protection = true
-  config.action_cable.url = 'ws://stage-wing.work/cable'
   config.action_cable.allowed_request_origins = ['http://stage-wing.com']
 end
